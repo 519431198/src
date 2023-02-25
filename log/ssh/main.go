0ffabf47413638d4a127e13c7bb96de1b2aa4cac
519431198 <519431198@qq.com>
2023-02-25 16:46:29 +0800

    Initial commit

--- log/ssh/main.go
+++ log/ssh/main.go
@@ -11,19 +11,26 @@ func main() {
 	var ip = "10.0.0.4"
 	var port = 22
 	config := &ssh.ClientConfig{
-		User:    "root",
-		Auth:    []ssh.AuthMethod{ssh.Password("123")},
-		Timeout: 10 * time.Second,
+		User:            "root",
+		Auth:            []ssh.AuthMethod{ssh.Password("123")},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         10 * time.Second,
 	}
-	addr := fmt.Sprintf("%cv:%v", ip, port)
+	addr := fmt.Sprintf("%v:%v", ip, port)
 
 	sshClient, err := ssh.Dial("tcp", addr, config)
 	if err != nil {
 		log.Fatal("unable to creat ssh conn")
 	}
 	//sshSession，这个 sshSession 是用来远程执行操作的
-	_, err = sshClient.NewSession()
+	sshSession, err := sshClient.NewSession()
 	if err != nil {
 		log.Fatal("unable to create ssh session")
 	}
+	shell := "cd /root;ls -l"
+	output, err := sshSession.CombinedOutput(shell)
+	if err != nil {
+		log.Fatal("error occurred:", err)
+	}
+	fmt.Println(string(output))
 }
