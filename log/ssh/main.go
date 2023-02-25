fc158cd4bc853921032a55065cbba3484ecc095a
519431198 <519431198@qq.com>
2023-02-25 16:02:50 +0800

    Initial commit

--- log/ssh/main.go
+++ log/ssh/main.go
@@ -2,44 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
+	"log"
 	"time"
 )
 
 func main() {
-
-}
-func connect(user, password, host string, port int) (*sftp.Client, error) {
-	var (
-		auth         []ssh.AuthMethod
-		addr         string
-		clientConfig *ssh.ClientConfig
-		sshClient    *ssh.Client
-		sftpClient   *sftp.Client
-		err          error
-	)
-	// get auth method
-	auth = make([]ssh.AuthMethod, 0)
-	auth = append(auth, ssh.Password(password))
-
-	clientConfig = &ssh.ClientConfig{
-		User:    user,
-		Auth:    auth,
-		Timeout: 30 * time.Second,
+	var ip = "10.0.0.4"
+	var port = 22
+	config := &ssh.ClientConfig{
+		User:    "root",
+		Auth:    []ssh.AuthMethod{ssh.Password("123")},
+		Timeout: 10 * time.Second,
 	}
+	addr := fmt.Sprintf("%cv:%v", ip, port)
 
-	// connet to ssh
-	addr = fmt.Sprintf("%s:%d", host, port)
-
-	if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
-		return nil, err
+	sshClient, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		log.Fatal("unable to creat ssh conn")
 	}
-
-	// create sftp client
-	if sftpClient, err = sftp.NewClient(sshClient); err != nil {
-		return nil, err
+	//sshSession，这个 sshSession 是用来远程执行操作的
+	_, err = sshClient.NewSession()
+	if err != nil {
+		log.Fatal("unable to create ssh session")
 	}
-
-	return sftpClient, nil
 }
