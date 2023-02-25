b769866b7263f9a6e564bdeb0b5636fbf2c6129d
519431198 <519431198@qq.com>
2023-02-25 17:51:05 +0800

    Initial commit

--- log/ssh1/main.go
+++ log/ssh1/main.go
@@ -8,8 +8,8 @@ import (
 	"time"
 )
 
-// ClientConfig 创建连接的配置的结构体
-type ClientConfig struct {
+// clientConfig 创建连接的配置的结构体
+type clientConfig struct {
 	Host       string      //ip
 	Port       int64       // 端口
 	Username   string      //用户名
@@ -18,7 +18,8 @@ type ClientConfig struct {
 	LastResult string      //最近一次运行的结果
 }
 
-func (cliConf *ClientConfig) createClient(host string, port int64, username, password string) {
+func (cliConf *clientConfig) createClient(host string, port int64, username, password string) {
+	//将传入的参数赋值给 ClientConfig 结构体字段
 	var client *ssh.Client
 	var err error
 	cliConf.Host = host
@@ -26,7 +27,8 @@ func (cliConf *ClientConfig) createClient(host string, port int64, username, pas
 	cliConf.Username = username
 	cliConf.Password = password
 	cliConf.Port = port
-	//一般传入四个参数：user，[]ssh.AuthMethod{ssh.Password(password)}, HostKeyCallback，超时时间，
+	//一般传入四个参数：user，[]ssh.AuthMethod{ssh.Password(password)}, HostKeyCallback，超时时间
+	//获取 ssh 所需要的配置
 	config := ssh.ClientConfig{
 		User: cliConf.Username,
 		Auth: []ssh.AuthMethod{ssh.Password(password)},
@@ -37,22 +39,28 @@ func (cliConf *ClientConfig) createClient(host string, port int64, username, pas
 	}
 	addr := fmt.Sprintf("%s:%d", cliConf.Host, cliConf.Port)
 	//获取client
+	/*
+		建立链接
+		调用 Dial 方法建立 ssh 连接,dial 方法三个参数和两个返回值
+		1.第一个为网络类型,例如使用面向连接的 TCP 协议
+		2.第二个 addr,为目标机器 ip 地址和端口
+		3.第三个 config 为连接生命的配置项
+		4.Dial 会返回一个 ssh 连接和错误类型
+	*/
 	if client, err = ssh.Dial("tcp", addr, &config); err != nil {
 		log.Fatalln("error occurred:", err)
 	}
 	cliConf.Client = client
 }
-func (cliConf *ClientConfig) RunShell(shell string) string {
-	var (
-		session *ssh.Session
-		err     error
-	)
-	//获取session，这个session是用来远程执行操作的
+func (cliConf *clientConfig) RunShell(shell string) string {
+	var session *ssh.Session
+	var err error
+	//获取 session 会话，这个session是用来远程执行操作的
 	session, err = cliConf.Client.NewSession()
 	if err != nil {
 		log.Fatalln("error occurred:", err)
 	}
-	//执行shell
+	//执行shell,返回一个 []byte 和一个错误类型
 	output, err := session.CombinedOutput(shell)
 	if err != nil {
 		log.Fatalln("error occurred:", err)
@@ -62,7 +70,9 @@ func (cliConf *ClientConfig) RunShell(shell string) string {
 	return cliConf.LastResult
 }
 func main() {
-	cliConf := new(ClientConfig)
+	//对结构体变量进行内存分配
+	var cliConf = new(clientConfig)
+	//cliConf := new(clientConfig)
 	cliConf.createClient("10.0.0.4", 22, "root", "123")
 	/*
 		可以看到我们这里每次执行一条命令都会创建一条session
