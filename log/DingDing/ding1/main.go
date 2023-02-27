3d2e1f905f4b50b54686ff868209365e1329290d
519431198 <519431198@qq.com>
2023-02-27 20:48:19 +0800

    Initial commit

--- log/DingDing/ding1/main.go
+++ log/DingDing/ding1/main.go
@@ -7,7 +7,8 @@ func main() {
 	d := ding.Webhook{
 		AccessToken: "f9f7e29c56a678facf1cafcc7bdd5263a4bef01e4335dc97436cb131c33bd439",
 		Secret:      "SEC6e004dca28307acaa0dbf36e601a9e3436b78c6d200f8ddd2b553ef9a409e854",
-		EnableAt:    true, // 开启艾特
+		//艾特所有人需要两个同时开启
+		EnableAt: true, // 开启艾特
 		//AtAll:       true, // 艾特所有人
 	}
 	_ = d.SendMessage("沙雕!", "18326147303")
