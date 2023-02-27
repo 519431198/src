b484c8addd8ea34d10540eb32c525139dd035de8
519431198 <519431198@qq.com>
2023-02-27 20:47:14 +0800

    Initial commit

--- log/DingDing/ding1/main.go
+++ log/DingDing/ding1/main.go
@@ -1,1 +1,14 @@
-package ding1
+package main
+
+import "github.com/wanghuiyt/ding"
+
+func main() {
+	//导入包 go get -u github.com/wanghuiyt/ding
+	d := ding.Webhook{
+		AccessToken: "f9f7e29c56a678facf1cafcc7bdd5263a4bef01e4335dc97436cb131c33bd439",
+		Secret:      "SEC6e004dca28307acaa0dbf36e601a9e3436b78c6d200f8ddd2b553ef9a409e854",
+		EnableAt:    true, // 开启艾特
+		//AtAll:       true, // 艾特所有人
+	}
+	_ = d.SendMessage("沙雕!", "18326147303")
+}
